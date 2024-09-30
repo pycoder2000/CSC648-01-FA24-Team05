882d4a680f53681a9443d6b4a615d922d2946531
Parth Desai <desaiparth2000@gmail.com>
2024-09-30 15:26:40 -0700

    Removed previous project and started a new one

--- application/backend/about_us/urls.py
+++
@@ -1,9 +0,0 @@
-# about_us/urls.py
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.about_us, name="about_us"),
-    path("member/<str:name>/", views.member_detail, name="member_detail"),
-]
