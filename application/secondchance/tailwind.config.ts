9de9b9f56ead4178e8ec68f1c646f46b7ab98683
Parth Desai <desaiparth2000@gmail.com>
2024-10-08 00:23:17 -0700

    Changed the base colors for the website

--- application/secondchance/tailwind.config.ts
+++ application/secondchance/tailwind.config.ts
@@ -9,8 +9,8 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        'airbnb': '#ff385c',
+        'airbnb-dark': '#d50027',
       },
     },
   },
