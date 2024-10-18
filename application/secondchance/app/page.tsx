c9133e6af10aa379bf72e65ecc38e82d7af9ada5
PFGrande <2002.pedrogrande@gmail.com>
2024-10-18 00:02:08 -0700

    removed placeholder text on home page

--- application/secondchance/app/page.tsx
+++ application/secondchance/app/page.tsx
@@ -4,10 +4,8 @@ import ProductList from "./components/properties/ProductList";
 export default function Home() {
   return (
     <main className="bg-black">
-      <p>
-        Server Sucessfully running
-      </p>
 
+      {/* Display products on home page */}
       <div className="mt-4 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-6">
           <ProductList />
       </div>
