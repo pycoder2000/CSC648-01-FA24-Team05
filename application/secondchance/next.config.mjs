90189853ce073f7a6e1c4949272fe668f63c2f19
PFGrande <2002.pedrogrande@gmail.com>
2024-10-17 21:53:15 -0700

    modified config file to allow nextjs to use images from other hosts

--- application/secondchance/next.config.mjs
+++ application/secondchance/next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: { // placeholder code to allow app to use images from other hosts
+        remotePatterns: [
+            {
+                // Allows app to use images from ALL hosts
+                protocol: 'https',
+                hostname: "**", // change this when we create a database
+            }
+        ]
+    }
+    
+
+
+};
+
+
 
 export default nextConfig;
