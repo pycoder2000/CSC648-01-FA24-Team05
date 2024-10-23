98148e5ee3e6b920508446e1aaed4440797ef8a1
NathVelarde <103010955+NathVelarde@users.noreply.github.com>
2024-10-23 14:06:52 -0700

    Category (#12)

---
+++ application/secondchance/app/components/categories/Categories.tsx
@@ -0,0 +1,39 @@
+const Categories = () => {
+    return (
+        <div className="pt-3 cursor-pointer pb-6 flex items-center space-x-12">
+            <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100">
+                Clothes
+            </div>
+
+            <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100">
+                Electronics
+            </div>
+
+            <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100">
+                Tools
+            </div>
+
+            <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100">
+                Furniture
+            </div>
+
+            <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100">
+                Sport Equipment
+            </div>
+
+            <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100">
+                Toys
+            </div>
+
+            <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100">
+                Vehicles
+            </div>
+
+            <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100">
+                Help
+            </div>
+        </div>
+    )
+}
+
+export default Categories;
--- application/secondchance/app/page.tsx
+++ application/secondchance/app/page.tsx
@@ -1,14 +1,22 @@
 import Image from "next/image";
 import ProductList from "./components/properties/ProductList";
+import Categories from "./components/categories/Categories";
 
 export default function Home() {
   return (
     <main className="bg-black">
 
+      {/*categories */}
+      <div className="max-w-[1500px] mx-auto px-6">
+        <Categories />
+      </div>
+
       {/* Display products on home page */}
       <div className="mt-4 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-6">
           <ProductList />
       </div>
+
+      
     </main>
     
 
