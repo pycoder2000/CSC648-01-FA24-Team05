459be6f15f058be2706f721e7b23e8e93a95ead7
Hsin Ying Tsai <yuko911018@gmail.com>
2024-10-22 21:26:09 -0700

    Added UserNav and AddPropertyButton and bright them

--- application/secondchance/app/components/navbar/SearchFilters.tsx
+++ application/secondchance/app/components/navbar/SearchFilters.tsx
@@ -1,24 +1,24 @@
 const SearchFilters = () =>{
     return(
-        <div className="h-[64px] flex flex-row items-center justify-between border rounded-full">
+        <div className="h-[48px] lg:h-[64] flex flex-row items-center justify-between border rounded-full">
             <div className="hidden lg:block">
                 <div className="flex flex-row items-center justify-between">
-                    <div className="cursor-pointer w-[250px] h-[64px] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100 ">
+                    <div className="cursor-pointer w-[250px] h-[48px] lg:h-[64] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100 ">
                         <p className="text-xs front-semibold text-black"> Where </p>
                         <p className="text-sm text-black">Wanted location</p>
                     </div>
 
-                    <div className="cursor-pointer h-[64px] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100 ">
+                    <div className="cursor-pointer h-[48px] lg:h-[64] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100 ">
                         <p className="text-xs front-semibold text-black"> Check in </p>
                         <p className="text-sm text-black">Add dates</p>
                     </div>
 
-                    <div className="cursor-pointer h-[64px] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100 ">
+                    <div className="cursor-pointer h-[48px] lg:h-[64] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100 ">
                         <p className="text-xs front-semibold text-black"> Check out </p>
                         <p className="text-sm text-black">Add dates</p>
                     </div>
 
-                    <div className="cursor-pointer h-[64px] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100 ">
+                    <div className="cursor-pointer h-[48px] lg:h-[64] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100 ">
                         <p className="text-xs front-semibold text-black"> who </p>
                         <p className="text-sm text-black">Add guests</p>
                     </div>
@@ -26,7 +26,7 @@ const SearchFilters = () =>{
                 </div>
             </div>
             <div className="p-2">
-                <div className="cursor-pointer p-4 bg-airbnb hover:bg-airbnb-dark transition rounded-full text-black">
+                <div className="cursor-pointer p-2 lg:p-4 bg-airbnb hover:bg-airbnb-dark transition rounded-full text-black">
                 <svg 
                 viewBox="0 0 32 32" style={{display:'block', fill:'none', height:'16px', width:'16px', stroke:'currentColor', strokeWidth:'4',overflow:'visible'}} 
                 aria-hidden="true" role="presentation" focusable="false">
