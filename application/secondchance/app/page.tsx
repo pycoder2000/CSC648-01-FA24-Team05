5f277b5cb117f57bd2c97a5708c3d968859ec90e
Parth Desai <desaiparth2000@gmail.com>
2024-10-25 16:26:42 -0700

    Added ItemCard for Front Page

--- application/secondchance/app/page.tsx
+++ application/secondchance/app/page.tsx
@@ -1,15 +1,14 @@
 import Categories from "./components/categories/Categories";
-// import PropertyList from "./components/properties/PropertyList";
+import ItemList from "./components/items/ItemList";
 
 export default function Home() {
   return (
     <main className="w-full mx-auto px-6">
       <Categories />
 
-      {/*
       <div className="mt-4 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-6">
-        <PropertyList />
-      </div> */}
+        <ItemList />
+      </div>
     </main>
   );
 }
