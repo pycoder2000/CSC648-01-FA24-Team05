56cd0f9661d018c9b24c1c97c3babeede08fa79e
Parth Desai <desaiparth2000@gmail.com>
2024-10-25 18:24:31 -0700

    Added Search Modal to get Search Functionality (#19)

---
+++ application/secondchance/app/hooks/useSearchModal.ts
@@ -0,0 +1,39 @@
+import { create } from "zustand";
+
+export type SearchQuery = {
+  location: string | undefined;
+  startDate: Date | undefined;
+  endDate: Date | undefined;
+  condition: string | undefined;
+  priceMin: number | undefined;
+  priceMax: number | undefined;
+  category: string;
+};
+
+interface SearchModalStore {
+  isOpen: boolean;
+  step: string;
+  open: (step: string) => void;
+  close: () => void;
+  query: SearchQuery;
+  setQuery: (query: SearchQuery) => void;
+}
+
+const useSearchModal = create<SearchModalStore>((set) => ({
+  isOpen: false,
+  step: "",
+  open: (step) => set({ isOpen: true, step: step }),
+  close: () => set({ isOpen: false }),
+  setQuery: (query: SearchQuery) => set({ query: query }),
+  query: {
+    location: undefined,
+    startDate: undefined,
+    endDate: undefined,
+    condition: '',
+    priceMin: undefined,
+    priceMax: undefined,
+    category: "",
+  },
+}));
+
+export default useSearchModal;
