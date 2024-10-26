006c5a778e3e71fd480bc62ccb804128d8a49f7e
Parth Desai <desaiparth2000@gmail.com>
2024-10-25 19:17:36 -0700

    Added Detailed Seller's Page

---
+++ application/secondchance/app/components/buttons/ContactButton.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import useLoginModal from "../hooks/useLoginModal";
+import { useRouter } from "next/navigation";
+import apiService from "../services/apiService";
+
+interface ContactButtonProps {
+    userId: string | null;
+    sellerId: string;
+}
+
+const ContactButton: React.FC<ContactButtonProps> = ({
+    userId,
+    sellerId
+}) => {
+    const loginModal = useLoginModal();
+    const router = useRouter();
+
+    const startConversation = async () => {
+        if (userId) {
+            const conversation = await apiService.get(`/api/chat/start/${sellerId}/`)
+
+            if (conversation.conversation_id) {
+                router.push(`/inbox/${conversation.conversation_id}`)
+            }
+        } else {
+            loginModal.open();
+        }
+    }
+
+    return (
+        <div
+            onClick={startConversation}
+            className="mt-6 py-4 px-6 cursor-pointer bg-secondchance text-white rounded-xl hover:bg-secondchance-dark transition"
+        >
+            Contact
+        </div>
+    )
+}
+
+export default ContactButton;
---
+++ application/secondchance/app/sellers/[id]/page.tsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+
+import ContactButton from "@/app/components/buttons/ContactButton";
+import ItemList from "@/app/components/items/ItemList";
+import { getUserId } from "@/app/lib/actions";
+import apiService from "@/app/services/apiService";
+
+const SellerDetailPage = async ({ params }: { params: { id: string } }) => {
+  const seller = await apiService.get(`/api/auth/${params.id}`);
+  const userId = await getUserId();
+
+  return (
+    <main className="max-w-[1500px] mx-auto px-6 pb-6">
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
+        <aside className="col-span-1 mb-4">
+          <div className="flex flex-col items-center p-6 rounded-xl border border-gray-300 shadow-xl">
+            <Image
+              src={seller.avatar_url}
+              width={200}
+              height={200}
+              alt="Seller"
+              className="rounded-full"
+            />
+
+            <h1 className="mt-6 text-2xl">{seller.name}</h1>
+
+            {userId != params.id && (
+              <ContactButton userId={userId} sellerId={params.id} />
+            )}
+          </div>
+        </aside>
+
+        <div className="col-span-1 md:col-span-3 pl-0 md:pl-6">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            <ItemList seller_id={params.id} />
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default SellerDetailPage;
