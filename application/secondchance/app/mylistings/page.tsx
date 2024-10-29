281aeca97d5ed6c14b4c03e6a0326e673d3a5783
Parth Desai <desaiparth2000@gmail.com>
2024-10-28 17:02:49 -0700

    Added a favorites page

--- application/secondchance/app/mylistings/page.tsx
+++ application/secondchance/app/mylistings/page.tsx
@@ -5,10 +5,12 @@ const MyListingsPage = async () => {
   const userId = await getUserId();
 
   return (
-    <main className="w-full mx-auto px-6 pb-6">
-      <h1 className="my-6 text-2xl">My Listings</h1>
+    <main className="w-full mx-auto px-6 pb-12">
+      <div className="flex justify-between items-center my-8">
+        <h1 className="text-3xl font-bold text-gray-800">My Listings</h1>
+      </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         <ItemList seller_id={userId} />
       </div>
     </main>
