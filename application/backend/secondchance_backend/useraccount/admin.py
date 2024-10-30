7b5dd0831c465b2672c768ca32fc38e8476b959f
Parth Desai <desaiparth2000@gmail.com>
2024-10-30 10:19:39 -0700

    Modified Seller Page to add more details (#42)

--- application/backend/secondchance_backend/useraccount/admin.py
+++ application/backend/secondchance_backend/useraccount/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 
 from .models import User
 
-admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("email", "name", "phone", "city", "state")
+    search_fields = ("email", "name", "phone")
