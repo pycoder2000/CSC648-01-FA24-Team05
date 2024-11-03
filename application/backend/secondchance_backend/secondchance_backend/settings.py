2ca4158bc24120f4fe158135ddf3041549c9fe90
Parth Desai <desaiparth2000@gmail.com>
2024-11-02 23:23:57 -0700

    Modified settings.py

--- application/backend/secondchance_backend/secondchance_backend/settings.py
+++ application/backend/secondchance_backend/secondchance_backend/settings.py
@@ -172,7 +172,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
