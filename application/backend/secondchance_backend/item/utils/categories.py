b552b5977c51d71cdd3a3d959419263bb748b248
PFGrande <2002.pedrogrande@gmail.com>
2024-12-06 18:03:53 -0800

    added functions to retrieve categories rented and listed by user

---
+++ application/backend/secondchance_backend/item/utils/categories.py
@@ -0,0 +1,41 @@
+from item.models import Rental
+from item.models import Item
+
+def get_user_rented_categories(user):
+    """
+    Get the categories of items that a user has rented from
+    
+    args: 
+    user instance
+    used to get items rented by user
+    
+    
+    return: list of categories
+    rtype: list?
+    """
+    # convert QuerySet to list before returning
+    return list(
+        Rental.objects.filter(rentals__created_by=user)
+        .values_list('category', flat=True) # returns desired field 
+        .distinct() # eliminate duplicate categories
+    )
+    
+    
+    
+def get_user_listed_categories(user):
+    """
+    Get the categories of items that a user has listed
+    
+    args: 
+    user instance
+    used to get items listed by user
+    
+    
+    return: list of categories
+    rtype: list?
+    """
+    return list(
+        Item.objects.filter(seller=user)
+        .values_list('category', flat=True)
+        .distinct()
+    )
